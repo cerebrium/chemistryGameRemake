cbee3dd7f8b7e83c9498bb16cce2d9bec4a7b4be
cerebrium <nicholas.m.shankland@gmail.com>
2020-02-04 13:21:56 -0800

    got everything working

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ let elementName = ''
 let currX = 0
 let currY = 0
 
+// interval that sets the current elements coordinates equal to the mouse coordinates every 100 miliseconds
+var moveTheTarget = (elementName) => {
+    if (elementName) {
+        document.getElementById(`${elementName}`).style.left = currX + 'px'
+        document.getElementById(`${elementName}`).style.top = currY + 'px'
+    }
+}
 
 // making the elements able to move
 document.addEventListener('mousedown', (ev) => {
@@ -76,38 +83,37 @@ document.addEventListener('mousedown', (ev) => {
         document.addEventListener('mousemove', (ev) => {
             currX = ev.clientX
             currY = ev.clientY
-        })
-        // interval that sets the current elements coordinates equal to the mouse coordinates every 100 miliseconds
-        let moveTheTarget = setInterval(() => {
-            document.getElementById(`${elementName}`).style.left = currX + 'px'
-            document.getElementById(`${elementName}`).style.top = currY + 'px'
-        }, 100)   
-           
-        // clears the interval so element stops moving, and sets the target to '' so next element can be selected
-        document.addEventListener('mouseup', (e) => {
-            elementsArray.forEach((ele, index) => {
-                if (ele.id === elementName) {
-                    ele.currentX = currX
-                    ele.currentY = currY 
-                }
-                if (ele.id !== elementName) {
-                    if ((Math.abs(ele.currentX-currX)) < 70 && (Math.abs(ele.currentY-currY)) < 70) {
-                        if (ele.bonds < ele.bondsLimit) {
-                            drawLine(elementName, ele.id)
-                            ele.bonds
-                            elementsArray.forEach((ele, index) => {
-                                if (ele.id === elementName) {
-                                    ele.bonds
-                                }
-                            })
-                        }
-                    }
-                }
-            })
-            clearInterval(moveTheTarget)
+            setInterval(moveTheTarget(elementName), 100)
         })
     }
 })
+
+// clears the interval so element stops moving, and sets the target to '' so next element can be selected
+document.addEventListener('mouseup', (e) => {
+    let otherElement = ''
+    elementsArray.forEach((ele, index) => {
+        if (ele.id === elementName) {
+            ele.currentX = currX
+            ele.currentY = currY  
+        }
+        if (ele.id !== elementName) {
+            if ((Math.abs(ele.currentX-currX)) < 70 && (Math.abs(ele.currentY-currY)) < 70) {
+                if (ele.bonds < ele.bondsLimit) {
+                    otherElement = ele.id
+                }
+            }
+        }
+    })
+    // clear the interval
+    clearInterval(moveTheTarget)
+    if (elementName && otherElement) {
+        drawLine(elementName, otherElement)
+    }
+
+    // set the target to nothing
+    elementName = ''
+})
+
 
 // END OF MOVEMENT SECTION
 
@@ -120,11 +126,6 @@ let drawLine = (atom1, atom2) => {
     let y1 = document.getElementById(`${atom1}`).getBoundingClientRect().top + 20
     let x2 = document.getElementById(`${atom2}`).getBoundingClientRect().right + 15
     let y2 = document.getElementById(`${atom2}`).getBoundingClientRect().top + 20
-
-    console.log('x1: ', x1)
-    console.log('x2: ', x2)
-    console.log('y1: ', y1)
-    console.log('y2: ', y2)
 
     // solve for distance between the two center points for the divs
     let lineLength = Math.sqrt(((x2-x1)**2)+((y2-y1)**2))
@@ -141,17 +142,14 @@ let drawLine = (atom1, atom2) => {
     console.log('rotation is: ', lineAngle)
     // introducing cases for placement of the lines
     if (x2 > x1 && y2 > y1) {
-        console.log('case 1')
         // down a little to left lot
         document.getElementById(`${x1}${x2}`).style.left = (x1-50) + 'px'
         document.getElementById(`${x1}${x2}`).style.top = (y1+16) + 'px'
     } else if (x1 > x2 && y2 > y1) {
-        console.log('case 2')
         // down, left a little
         document.getElementById(`${x1}${x2}`).style.left = (x1-80) + 'px'
         document.getElementById(`${x1}${x2}`).style.top = (y1+15) + 'px'
     } else if (x1 > x2 && y1 > y2) {
-        console.log('case 3')
         // up a little and to right little
         document.getElementById(`${x1}${x2}`).style.left = (x1-85) + 'px'
         document.getElementById(`${x1}${x2}`).style.top = (y1-15) + 'px'
@@ -159,8 +157,18 @@ let drawLine = (atom1, atom2) => {
         // down a little
         document.getElementById(`${x1}${x2}`).style.left = (x1-50) + 'px'
         document.getElementById(`${x1}${x2}`).style.top = (y1-20) + 'px'
-        console.log('case 4')
     }
+
+    elementsArray.forEach((ele, index) => {
+        console.log('farts');
+        if (ele.id === atom1) {
+            ele.bonds++
+        }
+        if (ele.id === atom2) {
+            ele.bonds++
+        }
+    })
+    console.log(elementsArray)
 }
 
 
